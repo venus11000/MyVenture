e5bf6a5be10568f9234cfcf04d99b751ca66179d
Venugopal <venugopala11000@gmail.com>
2021-09-05 02:02:27 +0530

    Routes for Category, sub Category and product

--- client/src/containers/routes.js
+++ client/src/containers/routes.js
@@ -7,6 +7,15 @@ import ProductDetails from "./ProductDetails";
 import Search from "./Search";
 import Login from "./Login";
 import Register from "./Register";
+import CreateCategory from "./CategoryCRUD/CreateCategory";
+import ListCategory from "./CategoryCRUD/ListCategories";
+import EditCategory from "./CategoryCRUD/EditCategory";
+import CreateSubCategory from "./SubCategoryCRUD/CreateSubCategory";
+import EditSubCategory from "./SubCategoryCRUD/EditSubCategories";
+import ListSubCategory from "./SubCategoryCRUD/ListSubCategories";
+import CreateProduct from "./ProductCRUD/CreateProduct";
+import EditProduct from "./ProductCRUD/EditProduct";
+import ListProduct from "./ProductCRUD/ListProducts";
 
 const App = () => {
     return (
@@ -21,6 +30,15 @@ const App = () => {
                         <Route path="/search" exact component={Search} />
                         <Route path="/login" exact component={Login} />
                         <Route path="/register" exact component={Register} />
+                        <Route path="/category/create" exact component={CreateCategory} />
+                        <Route path="/category/edit/:id" exact component={EditCategory} />
+                        <Route path="/category/list" exact component={ListCategory} />
+                        <Route path="/sub-category/create" exact component={CreateSubCategory} />
+                        <Route path="/sub-category/edit/:id" exact component={EditSubCategory} />
+                        <Route path="/sub-category/list" exact component={ListSubCategory} />
+                        <Route path="/product/create" exact component={CreateProduct} />
+                        <Route path="/product/edit/:id" exact component={EditProduct} />
+                        <Route path="/product/list" exact component={ListProduct} />
                     </div>
                 </Switch>
                 <Footer />
