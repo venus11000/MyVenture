531bda16bab2372fb4f818c650dee1a8e46feada
Venugopal <venugopala11000@gmail.com>
2021-09-13 01:27:37 +0530

    Book Appointment in Product Desc page

--- client/src/containers/ProductDetails/ProductDetails.js
+++ client/src/containers/ProductDetails/ProductDetails.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import { createAPpointment } from "../../core/appointment";
 import ProductImageCarosel from "./ProductImageCarosel";
 
 const ProductDetails = ({ product }) => {
+    const [appointment, setAppointment] = useState();
+
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    const bookAppointment = (productId) => {
+        let payload = {
+            productId,
+            userId: user._id
+        }
+        createAPpointment(payload)
+            .then((appointment) => {
+                setAppointment(appointment);
+            })
+    }
     return (
         <div className="product-details__card">
             <div className="product-details__image-container">
@@ -22,6 +38,17 @@ const ProductDetails = ({ product }) => {
                         <div>Area Occupied: {product.areaOccupied}</div>
                     </div>
                 </div>
+                {user && <div className="btn-primary" onClick={() => bookAppointment(product._id)}>Book Appointment</div>}
+                {appointment && <button
+                    className="btn-primary-outlined"
+                    style={{
+                        color: appointment._id ? "green" : "#d8232a",
+                        borderColor: appointment._id ? "green" : "#d8232a"
+                    }}
+                    disabled={true}
+                >
+                    {appointment._id ? "Appointment booked!" : "Error while booking appointment!"}
+                </button>}
                 {/* <Link className="btn-primary" to={"product-details__details?productId=" + product._id}>View More Details</Link> */}
             </div>
         </div>
